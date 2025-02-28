1b7709bb783ec8a9569deb26b2bdc84e25d8f114
iamdevkach <achyuth@softqlud.com>
2025-02-27 19:12:56 -0600

    Ip pojo

---
+++ src/main/java/com/invas/enhanced/fc/bert/model/IPAddress.java
@@ -0,0 +1,12 @@
+package com.invas.enhanced.fc.bert.model;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class IPAddress {
+
+    private ArrayList<String> ipv6;
+    private String ipv4;
+}
