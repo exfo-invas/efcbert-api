cd4053aa95ad650f19e2ad9fa25ed9b68bd68489
iamdevkach <achyuth@softqlud.com>
2025-02-27 19:06:01 -0600

    Refactor name

--- src/main/java/com/invas/enhanced/fc/bert/service/TelnetServiceImpl.java
+++ src/main/java/com/invas/enhanced/fc/bert/service/TelnetServiceImpl.java
@@ -1,45 +1,50 @@
 package com.invas.enhanced.fc.bert.service;
 
+import com.invas.enhanced.fc.bert.config.FetchIPAddress;
 import com.invas.enhanced.fc.bert.model.ConnectionResponse;
-import com.invas.enhanced.fc.bert.utils.TelnetConfigUtils;
+import com.invas.enhanced.fc.bert.model.IPAddress;
+import com.invas.enhanced.fc.bert.utils.TelnetConfigUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.lang.constant.Constable;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class TelnetServiceImpl implements TelnetService {
 
-    private final TelnetConfigUtils telnetConfigUtils;
+    private final TelnetConfigUtil telnetConfigUtil;
+    private final FetchIPAddress fetchIPAddress;
 
     @Override
     public ConnectionResponse getConnection(String localIpaddress, int port) {
-        telnetConfigUtils.getConnection(localIpaddress, 23);
+        telnetConfigUtil.getConnection("localhost", 23);
 
         return new ConnectionResponse(
-                telnetConfigUtils.getStatus(),
+                telnetConfigUtil.getStatus(),
                 localIpaddress,
-                telnetConfigUtils.getStatus() ? "Connection established successfully" : "Connection failed"
+                telnetConfigUtil.getStatus() ? "Connection established successfully" : "Connection failed"
         );
     }
 
+    public IPAddress getIPAddress() {
+        return fetchIPAddress.getIP();
+    }
+
     public String disconnect() {
-        telnetConfigUtils.disconnect();
-        return !telnetConfigUtils.getStatus() ? "true" : "false";
+        telnetConfigUtil.disconnect();
+        return !telnetConfigUtil.getStatus() ? "true" : "false";
     }
 
     public String status() {
-        log.info(telnetConfigUtils.getAddress());
-        return telnetConfigUtils.getStatus() ? "true" : "false";
+        log.info(telnetConfigUtil.getAddress());
+        return telnetConfigUtil.getStatus() ? "true" : "false";
     }
 
 
     public String sendCommand(String command) {
         try {
-            return telnetConfigUtils.readAndWriteData(command);
+            return telnetConfigUtil.sendCommand(command);
         } catch (Exception e) {
             return "Failed to send command";
         }
