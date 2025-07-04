a44c21d37fd54fa55f12bb7471fa20594ea3314d
Achyuth <imdevkach@gmail.com>
2025-07-03 20:56:04 -0500

    refactor telnetClient

--- src/main/java/com/invas/enhanced/fc/bert/utils/ScpiTelnetHandler.java
+++ src/main/java/com/invas/enhanced/fc/bert/utils/ScpiTelnetHandler.java
@@ -3,12 +3,14 @@ package com.invas.enhanced.fc.bert.utils;
 import com.invas.enhanced.fc.bert.config.TelnetConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class ScpiTelnetHandler {
 
