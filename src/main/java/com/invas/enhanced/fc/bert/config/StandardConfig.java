32b7800604163dc1cc98f2db143ccd3bd3ae7658
achyuth yallambhotla <achyuth@softqlud.com>
2025-11-02 19:17:22 -0600

    effect Objectifing fcRate and frameRate

--- src/main/java/com/invas/enhanced/fc/bert/config/StandardConfig.java
+++ src/main/java/com/invas/enhanced/fc/bert/config/StandardConfig.java
@@ -1,5 +1,6 @@
 package com.invas.enhanced.fc.bert.config;
 
+import com.invas.enhanced.fc.bert.model.event.StandardTestResponse;
 import lombok.Data;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +8,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StandardConfig {
 
-    String fcRate;
-    double frameSize;
-
+    StandardTestResponse standardTestResponse;
 }
