824556d7715232ea7d1a17fe5c9393bec9566759
achyuth yallambhotla <achyuth@softqlud.com>
2025-12-07 18:27:49 -0600

    Hours Counter

--- src/main/java/com/invas/enhanced/fc/bert/model/event/HourlyCounter.java
+++ src/main/java/com/invas/enhanced/fc/bert/model/event/HourlyCounter.java
@@ -1,19 +1,19 @@
 package com.invas.enhanced.fc.bert.model.event;
 
 public class HourlyCounter {
-    private int hoursElapsed;
+    private String hoursElapsed;
     private boolean isReady;
 
-    public HourlyCounter(int hoursElapsed, boolean isReady) {
+    public HourlyCounter(String hoursElapsed, boolean isReady) {
         this.hoursElapsed = hoursElapsed;
         this.isReady = isReady;
     }
 
-    public int getHoursElapsed() {
+    public String getHoursElapsed() {
         return hoursElapsed;
     }
 
-    public void setHoursElapsed(int hoursElapsed) {
+    public void setHoursElapsed(String hoursElapsed) {
         this.hoursElapsed = hoursElapsed;
     }
 
