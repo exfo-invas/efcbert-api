5c65374fc2474a55b8b572520f9f9db1924c648a
achyuth yallambhotla <achyuth@softqlud.com>
2025-12-07 18:27:15 -0600

    Decimal handler

---
+++ src/main/java/com/invas/enhanced/fc/bert/utils/DecimalHandlerUtil.java
@@ -0,0 +1,32 @@
+package com.invas.enhanced.fc.bert.utils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class DecimalHandlerUtil {
+
+    public static BigDecimal ifNullReturnZero(String value) {
+        if (value == null) {
+            return BigDecimal.ZERO;
+        } else {
+            return roundToTwoDecimalPlaces(new BigDecimal(value));
+        }
+    }
+
+    private static BigDecimal roundToTwoDecimalPlaces(BigDecimal value) {
+        return value.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public static BigDecimal valuePercentage(BigDecimal value, BigDecimal percentage) {
+        return value.multiply(percentage).divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP);
+    }
+
+    public static BigDecimal percentageOfBigDecimal(BigDecimal part, BigDecimal total) {
+        if (total.compareTo(BigDecimal.ZERO) == 0) return BigDecimal.ZERO;
+        // new calculation: part * (total / 100)
+        return part
+                .divide(total, 10, RoundingMode.HALF_UP)   // part / total
+                .multiply(BigDecimal.valueOf(100))         // * 100
+                .setScale(2, RoundingMode.HALF_UP);        // final formatting to 2 decimal places
+    }
+}
